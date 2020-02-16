d99a8f53475c35339cc8a51cd11f632f130fe48c
Ada <ada.97.1405@gmail.com>
2020-02-16 12:44:38 -0500

    Primer commit

---
+++ vistas/paginas/modulos/footer.php
@@ -0,0 +1,136 @@
+<!--=====================================
+FOOTER
+======================================-->
+
+<footer class="container-fluid py-5 d-none d-md-block">
+	
+	<div class="container">
+		
+		<div class="row">
+
+		<!-- GRID CATEGORÍAS FOOTER -->
+			
+			<div class="col-md-7 col-lg-6">
+				
+				<div class="p-1 bg-white gridFooter">
+
+					<div class="container p-0">
+
+						<div class="d-flex">
+
+							<div class="d-flex flex-column columna1">
+							
+								<figure class="p-2 m-0 photo1" vinculo="categorias.html">
+									
+									<p class="text-uppercase p-1 p-md-2 p-xl-1 small">Suramérica</p>
+
+								</figure>
+
+								<figure class="p-2 m-0 photo2" vinculo="categorias.html">
+									
+									<p class="text-uppercase p-1 p-md-2 p-xl-1 small">Africa</p>
+
+								</figure>								
+
+							</div>
+
+							<div class="d-flex flex-column flex-fill columna2">
+
+								<div class="d-block d-md-flex">
+
+									<figure class="p-2 m-0 flex-fill photo3" vinculo="categorias.html">
+
+										<p class="text-uppercase p-1 p-md-2 p-xl-1 small">Centromérica</p>
+										
+									</figure>
+
+									<figure class="p-2 m-0 flex-fill photo4" vinculo="categorias.html">
+										
+										<p class="text-uppercase p-1 p-md-2 p-xl-1 small">Europa</p>
+
+									</figure>
+
+								</div>
+
+								<figure class="p-2 m-0 photo5" vinculo="categorias.html">
+
+									<p class="text-uppercase p-1 p-md-2 p-xl-1 small">Norteamérica</p>
+									
+								</figure>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+					
+			</div>
+
+			<div class="d-none d-lg-block col-lg-1 col-xl-2"></div>
+
+			<!-- NEWLETTER -->
+
+			<div class="col-md-5 col-lg-5 col-xl-4 pt-5">
+				
+				<h6>Inscríbete en nuestro newletter:</h6>
+
+				<div class="input-group my-4">
+					
+					<input type="text" class="form-control" placeholder="Ingresa tu Email">
+
+					<div class="input-group-append">
+						
+						<span class="btn btn-info text-white">Inscribirse</span>
+
+					</div>
+
+				</div>
+
+				<div class="p-0 w-100 pt-2">
+				
+					<ul class="d-flex justify-content-left p-0">
+						
+						
+					<li>
+						<a href="https://www.facebook.com" target="_blank">
+						<img src="vistas/img/facebook.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+							
+					</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+							<img src="vistas/img/insta.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+						<img src="vistas/img/twit.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+							<img src="vistas/img/yu.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+							<img src="vistas/img/anap.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+					</ul>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</footer>
---
+++ vistas/paginas/modulos/redes-sociales-movil.php
@@ -0,0 +1,42 @@
+<!--=====================================
+REDES SOCIALES PARA MÓVIL
+======================================-->
+
+<div class="d-block d-md-none redes redesMovil p-0 bg-white w-100 pt-2">
+				
+	<ul class="d-flex justify-content-center p-0">
+		
+	<li>
+						<a href="https://www.facebook.com" target="_blank">
+						<img src="vistas/img/facebook.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+							
+					</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+							<img src="vistas/img/insta.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+						<img src="vistas/img/twit.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+							<img src="vistas/img/yu.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+
+					<li>
+						<a href="#" target="_blank">
+							<img src="vistas/img/anap.png" width="40" heigth="40" style="margin: 0 5px 0 0">
+						</a>
+					</li>
+
+	</ul>
+
+</div>
